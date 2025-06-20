a0d91d74df540a9e335596c2d8725cf6363a4bf8
Federico Bravetti <federicobravetti802@gmail.com>
2025-06-20 13:16:27 +0200

    feat(turn_back_check): restore functionality

--- maze_oracle.py
+++ maze_oracle.py
@@ -32,8 +32,9 @@ class MazeCircuitInfo:
         return self.__num_qubits_in_max_path
     
 class MazeOracle(QuantumCircuit):
-    def __init__(self, maze_circuit_info: MazeCircuitInfo, max_path_length: int = None, turn_back_check: bool = False):
+    def __init__(self, maze_circuit_info: MazeCircuitInfo, turn_back_check: bool = False):
         self.__maze_circuit_info = maze_circuit_info
+        self.__turn_back_check = turn_back_check
         self.__num_ancillas = self.__maze_circuit_info.max_path_length
         if turn_back_check:
             self.__num_ancillas += self.__maze_circuit_info.max_path_length - 1
@@ -85,29 +86,32 @@ class MazeOracle(QuantumCircuit):
             quantum_circuit.barrier()
         return quantum_circuit
     
-    # # check if the nodes are equal 
-    # def __generate_turn_back_check_circuit(self):
-    #     last = self.__last_node
-    #     size = self.__node_bits_size
-    #     circ = QuantumCircuit((2 * size) + 1, name='Turn Back Check')
+    # check if the nodes are equal 
+    def __generate_turn_back_check_circuit(self):
+        # last = self.__last_node
+        size = self.__maze_circuit_info.bits_per_node
+        circ = QuantumCircuit((2 * size) + 1, name='Turn Back Check')
 
-    #     # first check: nodes are not equal
-    #     for i in range(size):
-    #         circ.cx(i, i + size)
-    #         circ.x(i + size)
-    #     circ.append(XGate().control(size), range(size, (2 * size) + 1))
-    #     circ.x((2 * size))
-    #     for i in range(size):
-    #         circ.x(i + size)
-    #         circ.cx(i, i + size)
+        # first check: nodes are not equal
+        for i in range(size):
+            circ.cx(i, i + size)
+            circ.x(i + size)
+        circ.append(XGate().control(size), range(size, (2 * size) + 1))
+        circ.x((2 * size))
+        for i in range(size):
+            circ.x(i + size)
+            circ.cx(i, i + size)
 
-    #     # second check: first node is equal to the last node
-    #     if last is not None:
-    #         circ.append(self.__node_to_binary(last), range(size))
-    #         circ.append(XGate().control(size), list(range(size)) + [size*2])
-    #         circ.append(self.__node_to_binary(last), range(size))
+        # second check: first node is equal to the last node
+        # if last is not None:
+        last_id = self.__maze_circuit_info.graph.end.id
+        circ.append(self.__node_to_binary(last_id), range(size))
+        circ.append(XGate().control(size), list(range(size)) + [size*2])
+        circ.append(self.__node_to_binary(last_id), range(size))
 
-    #     return circ
+        # circ.append(self.__node_to_binary(last), range(2*size))
+
+        return circ
 
     def __generate(self):
         full_edge_check = QuantumCircuit(self.__total_size, name='Full Edge Check')
@@ -115,8 +119,6 @@ class MazeOracle(QuantumCircuit):
 
         # add self-cycle to last node (for termination)
         edges.append(Edge(self.__maze_circuit_info.graph.end, self.__maze_circuit_info.graph.end))
-        for e in edges:
-            print(e)
         last_edge_check  = self.__generate_edge_check_circuit(filter(lambda e: e.end == self.__maze_circuit_info.graph.end, edges)) # only check edges containing the last node
         last_edge_check_ancilla = self.__maze_circuit_info.num_qubits_in_max_path + self.__maze_circuit_info.max_path_length - 1
         full_edge_check.append(last_edge_check, list(range(self.__maze_circuit_info.num_qubits_in_max_path - 2 * self.__maze_circuit_info.bits_per_node, self.__maze_circuit_info.num_qubits_in_max_path)) + [last_edge_check_ancilla])
@@ -130,12 +132,13 @@ class MazeOracle(QuantumCircuit):
             start_qubit    = s * self.__maze_circuit_info.bits_per_node
             full_edge_check.append(edge_check, list(range(start_qubit, start_qubit + 2 * self.__maze_circuit_info.bits_per_node)) + [self.__maze_circuit_info.num_qubits_in_max_path + s])
 
-        # turn_back_check = self.__generate_turn_back_check_circuit()
-        # for s in range(1, self.__max_path_length):
-        #     start_qubit    = s * self.__node_bits_size
-        #     previous_qubit = (s-1) * self.__node_bits_size
-        #     next_qubit     = (s+1) * self.__node_bits_size
-        #     full_edge_check.append(turn_back_check, list(range(previous_qubit, start_qubit)) + list(range(next_qubit, next_qubit + self.__node_bits_size)) + [self.__max_path_length + num_qubit_in_path + s - 1])
+        if self.__turn_back_check:
+            turn_back_check = self.__generate_turn_back_check_circuit()
+            for s in range(1, self.__maze_circuit_info.max_path_length):
+                start_qubit    = s * self.__maze_circuit_info.bits_per_node
+                previous_qubit = (s-1) * self.__maze_circuit_info.bits_per_node
+                next_qubit     = (s+1) * self.__maze_circuit_info.bits_per_node
+                full_edge_check.append(turn_back_check, list(range(previous_qubit, start_qubit)) + list(range(next_qubit, next_qubit + self.__maze_circuit_info.bits_per_node)) + [self.__maze_circuit_info.max_path_length + self.__maze_circuit_info.num_qubits_in_max_path + s - 1])
 
         self.append(full_edge_check, range(self.__total_size))
         self.append(ZGate().control(self.__num_ancillas - 1), range(self.__maze_circuit_info.num_qubits_in_max_path, self.__total_size))
